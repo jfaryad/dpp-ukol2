8fdbcb80c8570990653a881686e2c8142dbfabfa
Tomas Safarik <tomas.safarik@gmail.com>
2012-04-25 22:54:21 +0200

    Annotations refactored

---
+++ src/cz/cuni/mff/dpp/annotation/ParameterOption.java
@@ -0,0 +1,46 @@
+package cz.cuni.mff.dpp.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import cz.cuni.mff.dpp.ArgumentConverter;
+import cz.cuni.mff.dpp.DefaultArgumentConverterImpl;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ParameterOption {
+	
+    /**
+     * Jména volby (krátká i dlouhá)
+     * 
+     * @return
+     */
+    String[] names();
+    
+    String description() default "";
+    
+    /**
+     * To be displayed in generated help screen.
+     * 
+     * @return
+     */
+    String argumentName() default "";
+
+    boolean required() default false;
+
+    String[] dependentOn() default {};
+
+    String[] incompatibleWith() default {};
+    
+    boolean optionRequired() default false;
+    
+    boolean parameterRequired() default true;
+    
+    Class<? extends ArgumentConverter<?>> argumentConverter() default DefaultArgumentConverterImpl.class;
+    
+    String[] defaultParameter() default {};
+    
+    boolean multipleInvocation() default true;
+    
+    
+}
---
+++ src/cz/cuni/mff/dpp/annotation/SimpleOption.java
@@ -0,0 +1,20 @@
+package cz.cuni.mff.dpp.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SimpleOption {
+	
+	
+	String[] names();
+	
+	String description() default "";
+	
+	
+    String[] dependentOn() default {};
+
+    String[] incompatibleWith() default {};
+    
+}
