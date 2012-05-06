916e2ecf6e2b402f16bafb1e35e38c0a461b5095
Tomas Safarik <tomas.safarik@gmail.com>
2012-05-06 23:01:46 +0200

    Mírné vylepšení javadocu

--- src/cz/cuni/mff/dpp/impl/parser/DefaultCommandLineParser.java
+++ src/cz/cuni/mff/dpp/impl/parser/DefaultCommandLineParser.java
@@ -6,37 +6,23 @@ import cz.cuni.mff.dpp.impl.option.OptionsFactory;
 
 /**
  * 
- * Default {@link CommandLineParser} implementation.
- * 
- * Uses this command line algorithm:
- * 
- * 1. Target bean is create (must have parameterless public constructor)
- * 
- * 2. command line parsing: for every command line element is determined whether it is:
- * 
- * a) short option (starts with - and one character, e.g. -a),
- * 
- * b) united short option (starts with - and contains multiple characters, e.g. -abc)
- * 
- * c) long option (starts with -- a contains more characters, e.g. --output)
- * 
+ * Default {@link CommandLineParser} implementation. <br>
+ * Uses this command line algorithm: <br>
+ * 1. Target bean is create (must have parameterless public constructor) <br>
+ * 2. command line parsing: for every command line element is determined whether it is: <br>
+ * a) short option (starts with - and one character, e.g. -a), <br>
+ * b) united short option (starts with - and contains multiple characters, e.g. -abc) <br>
+ * c) long option (starts with -- a contains more characters, e.g. --output) <br>
  * d) option with parameter (normal option after character = and parameter, e.g. -f=tmp.txt or --file=tmt.txt or
- * -abf=tmp.txt, in this case option parameter belongs to last short option)
- * 
+ * -abf=tmp.txt, in this case option parameter belongs to last short option) <br>
  * e) option parameter (it is not option format and follows after option with required parameter without this parameter
- * (e.g. without =value))
- * 
- * e) common argument delimiter (--)
- * 
- * f) common argument (everything after -- and elements, that are not options and option parameters)
- * 
- * 3. validation:
- * 
+ * (e.g. without =value)) <br>
+ * e) common argument delimiter (--) <br>
+ * f) common argument (everything after -- and elements, that are not options and option parameters) <br>
+ * 3. validation: <br>
  * In this step is every option, option parameter and common argument validated (e.g. occurrences count, is option
- * permitted and so on...). If some validation fails appropriate exception is thrown.
- * 
- * 4. Target bean is population.
- * 
+ * permitted and so on...). If some validation fails appropriate exception is thrown. <br>
+ * 4. Target bean is population. <br>
  * Appropriate methods are invoked, fields are setted. Option parameters and common arguments are converted and
  * validated by costumer validators during this phase.
  * 
